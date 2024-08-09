4cd3964f3262b295e8259b5f4462b6498073e69b
waspflannel <jadenvar1@gmail.com>
2024-08-09 12:06:44 -0400

    removed unnessesary room node types. dungeons will be based on a new system

--- Assets/Scripts/NodeGraph/RoomNodeTypeSO.cs
+++ Assets/Scripts/NodeGraph/RoomNodeTypeSO.cs
@@ -8,8 +8,6 @@ public class RoomNodeTypeSO : ScriptableObject
 {
     public string roomNodeTypeName;
     public bool displayInNodeGraphEditor = true;
-    public bool isCorridor;
-    public bool isCorridorEW;
     public bool isEntrance;
     public bool isBossRoom;
     public bool isNone;
